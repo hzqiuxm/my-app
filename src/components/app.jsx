c0af48051ecf83340b769d3ca74596ec163cec67
qiuding <880807>
2018-12-18 12:01:08 +0800

    init app

---
+++ src/components/app.jsx
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+import logo from '../logo.svg'
+
+
+export default class App extends Component {
+
+    render() {
+        return (
+         <div>
+            <img className='logo' src={logo} alt="logo"/>
+             <p className='title'>my react companent</p>
+         </div>
+        );
+    }
+}
+
