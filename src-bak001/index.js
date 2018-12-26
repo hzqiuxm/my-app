27d82ca451a0afa3394d6dd46d8b216af65e5d17
菡萏如佳人(simon) <hzqiuxm@163.com>
2018-12-26 10:23:19 +0800

    add some setting

--- src-bak001/index.js
+++ src-bak001/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import App from './components/app/app'
-//import 'bootstrap'
+import App from './components/app'
+import './index.css'
 
 ReactDom.render(<App/>,document.getElementById('root'))
 
